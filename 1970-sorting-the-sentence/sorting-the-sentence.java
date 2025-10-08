4469a7be91d6deb01e54966f4f56b1a6b5901d86
Priyam Rajawat <rajawat.priyam2@gmail.com>
2025-10-08 20:12:10 +0530

    Time: 1 ms (39.47%) | Memory: 41.3 MB (89.16%) - LeetSync

---
+++ 1970-sorting-the-sentence/sorting-the-sentence.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String sortSentence(String s) {
+        String[] words = s.split(" ");
+        String[] ans = new String[words.length];
+        StringBuilder result = new StringBuilder();
+        int i = 0;
+        for(String word : words){
+            i = (int) (word.charAt(word.length() -1)- '0');
+            ans[i-1] = word.substring(0, word.length()-1);
+        }
+
+        for( i = 0; i < ans.length-1; i++){
+            result.append(ans[i] + " ");
+        }
+        result.append(ans[i]);
+        return result.toString();
+    }
+}
