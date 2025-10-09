4db0cf17381455a6271be5ee91dade8e55d169c6
Priyam Rajawat <rajawat.priyam2@gmail.com>
2025-10-09 21:16:03 +0530

    Time: 10 ms (10.62%) | Memory: 42.1 MB (30.46%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
@@ -1,16 +1,12 @@
 class Solution {
     public boolean halvesAreAlike(String s) {
-        s = s.toLowerCase();
         int count1 = 0; int count2 = 0;
-        for(int i = 0; i < s.length()/2; i++){
-            if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u') count1++;
+        int n = s.length()/2;
+        for(int i = 0; i < n; i++){
+            if(s.charAt(i + n) == 'a' || s.charAt(i + n) == 'e' || s.charAt(i + n) == 'i' || s.charAt(i + n) == 'o' || s.charAt(i + n) == 'u' || s.charAt(i + n) == 'A' || s.charAt(i + n) == 'E' || s.charAt(i + n) == 'I' || s.charAt(i + n) == 'O' || s.charAt(i + n) == 'U') count2++;
+            if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u' || s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' || s.charAt(i) == 'O' || s.charAt(i) == 'U') count1++;
         }
 
-        for(int i = s.length()/2; i < s.length() ; i++){
-            if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u') count2++;
-        }
-
-        if(count1 == count2) return true;
-        return false;
+        return count1 == count2;
     }
 }
