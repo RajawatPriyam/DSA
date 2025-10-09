c0b13028183e5874431068e0e2de82fe14ac7f82
Priyam Rajawat <rajawat.priyam2@gmail.com>
2025-10-09 21:21:21 +0530

    Time: 3 ms (90.58%) | Memory: 41.7 MB (76.85%) - LeetSync

--- 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
+++ 1823-determine-if-string-halves-are-alike/determine-if-string-halves-are-alike.java
@@ -3,8 +3,10 @@ class Solution {
         int count1 = 0; int count2 = 0;
         int n = s.length()/2;
         for(int i = 0; i < n; i++){
-            if(s.charAt(i + n) == 'a' || s.charAt(i + n) == 'e' || s.charAt(i + n) == 'i' || s.charAt(i + n) == 'o' || s.charAt(i + n) == 'u' || s.charAt(i + n) == 'A' || s.charAt(i + n) == 'E' || s.charAt(i + n) == 'I' || s.charAt(i + n) == 'O' || s.charAt(i + n) == 'U') count2++;
-            if(s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u' || s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' || s.charAt(i) == 'O' || s.charAt(i) == 'U') count1++;
+            char ch1 = s.charAt(i);
+            char ch2 = s.charAt(i+n);
+            if(ch2 == 'a' || ch2 == 'e' || ch2 == 'i' || ch2 == 'o' || ch2 == 'u' || ch2 == 'A' || ch2 == 'E' || ch2 == 'I' || ch2 == 'O' || ch2 == 'U') count2++;
+            if(ch1 == 'a' || ch1 == 'e' || ch1 == 'i' || ch1 == 'o' || ch1 == 'u' || ch1 == 'A' || ch1 == 'E' || ch1 == 'I' || ch1 == 'O' || ch1 == 'U') count1++;
         }
 
         return count1 == count2;
