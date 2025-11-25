9d3e078a20f2bda163c4a24308f24de9ff793a76
Priyam Rajawat <rajawat.priyam2@gmail.com>
2025-11-26 00:09:44 +0530

    Time: 10 ms (8.07%) | Memory: 45.4 MB (22.22%) - LeetSync

---
+++ 657-robot-return-to-origin/robot-return-to-origin.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int ver = 0; int hor = 0;
+        for(int i = 0; i < moves.length(); i++){
+            if(moves.charAt(i) == 'U') ver++;
+            else if(moves.charAt(i) == 'D') ver--;
+
+            if(moves.charAt(i) == 'L') hor++;
+            else if(moves.charAt(i) == 'R') hor--;
+        }
+        if(hor == 0 && ver == 0) return true;
+        return false;
+    }
+}
