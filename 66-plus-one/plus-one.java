5b05d95c01cae83eeaca8dff77cd200c67429482
Priyam Rajawat <rajawat.priyam2@gmail.com>
2026-03-27 22:53:33 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (93.52%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,21 +1,16 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int i = digits.length -1;
-        int carry = 1;
-        ArrayList<Integer> res = new ArrayList<>();
-        while( i >= 0 || carry > 0){
-            int sum = carry;
-
-            if(i >= 0)sum += digits[i--];
-            carry = sum/10;
-
-            res.add(sum%10);
+        for(int i = digits.length -1; i >= 0; i--){
+            if(digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
         }
-        Collections.reverse(res);
-        int[] arr = new int[res.size()];
-        for(i = 0; i < arr.length; i ++){
-            arr[i] = res.get(i);
-        }
-        return arr;
+
+        int[] newdigits = new int[digits.length+1];
+        newdigits[0] = 1;
+        return newdigits;
+
     }
 }
