214f51ea3fe238b116cd3e7d466f7664aaa1aaac
Priyam Rajawat <rajawat.priyam2@gmail.com>
2026-03-31 23:21:14 +0530

    Time: 16 ms (47.96%) | Memory: 47.9 MB (91.96%) - LeetSync

--- 477-total-hamming-distance/total-hamming-distance.java
+++ 477-total-hamming-distance/total-hamming-distance.java
@@ -1,13 +1,13 @@
 class Solution {
     public int totalHammingDistance(int[] nums) {
-        int total = 0;
+        int total = 0; int n = nums.length;
         for(int i = 0; i<32; i++){
             int ones = 0;
             for(int num : nums){
                 if(((num >> i) & 1) == 1) ones++;
             }
 
-            int zeroes = nums.length - ones;
+            int zeroes = n - ones;
             total += ones * zeroes;
         }
         return total;
