d4a220ea2c2ff028885b41958f04e1e53be79f4c
Priyam Rajawat <rajawat.priyam2@gmail.com>
2026-04-13 20:54:57 +0530

    Time: 2 ms (97.91%) | Memory: 42.6 MB (53.08%) - LeetSync

--- 728-self-dividing-numbers/self-dividing-numbers.java
+++ 728-self-dividing-numbers/self-dividing-numbers.java
@@ -3,17 +3,15 @@ class Solution {
         ArrayList<Integer> list = new ArrayList<>();
         for(int i = left; i <= right; i++){
             int temp = i;
-            boolean self = true;
 
             while(temp > 0){
                 int last = temp%10;
                 if(last == 0 || i % last != 0){
-                    self = false;
                     break;
                 }
                 temp /= 10;
             }
-            if(self) list.add(i);
+            if(temp == 0) list.add(i);
         }
         return list;
     }
